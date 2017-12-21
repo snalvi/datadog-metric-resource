18c75489e8ff27638a25f47eaca25bea902f09c6
Saman Alvi <salvi@pivotal.io>
2017-12-20 19:44:09 -0500

    Updating Gemfile to include Ruby
    Signed-off-by: Saman Alvi <salvi@pivotal.io>

--- spec/out_spec.rb
+++ spec/out_spec.rb
@@ -1,15 +1,14 @@
 require 'spec_helper'
 require 'json'
 
-
-Rspec.describe 'out' do
+RSpec.describe 'out' do
   let(:api_key) { 'api_key' }
   let(:metric_name) { 'metric_name' }
   let(:metric_value) { 'metric_value' }
 
   context 'when the api_key and metric_name and metric_value is provided' do
     it 'is able to make a successful HTTP request' do
-
+    	puts 'n/a'
     end
   end
 end
